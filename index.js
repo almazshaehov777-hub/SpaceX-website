24e0bae4ec8c02a6c7958f7ef69bf6f3d2290ef2
Almaz <Almazshaehov777@gmail.com>
2026-04-12 21:21:39 +0300

    исправлена первая страница для мобильных устройств и почти доделана версия для пк

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const button = document.getElementById('btn');
 const con = document.querySelector('.container');
+const menu = document.getElementById('imageMenu');
 
 button.addEventListener('click', () => {
     con.classList.toggle('active');
+    if(menu.src.endsWith("menuImage/right-arrow.png")){
+        menu.src = "menuImage/arrow-down-sign-to-navigate.png"
+    }
+    else{
+        menu.src = "menuImage/right-arrow.png"
+    }
 });
